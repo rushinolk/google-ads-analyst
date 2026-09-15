8204c59252fb1f0cf5812dec2adde00050b94200
rushinolk <arthurmachado7191@hotmail.com>
2026-09-15 16:01:07 -0300

    feat(sql): adiciona carga na fct

---
+++ sql/04_load_fct.sql
@@ -0,0 +1,33 @@
+INSERT INTO fct_anuncio (
+    id_anuncio,
+    id_campaign_name,
+    id_location,
+    id_device,
+    id_keyword,
+    clicks,
+    impressions,
+    cost,
+    leads,
+    conversions,
+    sale_amount,
+    ad_date
+)
+SELECT
+    s.ad_id,
+    dc.id_campaign_name,
+    dl.id_location,
+    dd.id_device,
+    dk.id_keyword,
+    COALESCE(s.clicks, 0),
+    COALESCE(s.impressions, 0),
+    COALESCE(s.cost, 0),
+    COALESCE(s.leads, 0),
+    COALESCE(s.conversions, 0),
+    COALESCE(s.sale_amount, 0),
+    s.ad_date
+FROM vw_staging_norm s
+JOIN dim_campaign_name dc ON dc.campaign_name = s.campaign_name
+JOIN dim_location      dl ON dl.name_location  = s.location
+JOIN dim_device        dd ON dd.device         = s.device
+JOIN dim_keyword       dk ON dk.keyword        = s.keyword
+ON CONFLICT (id_anuncio) DO NOTHING;
